c84d43e652b530feb50d195dfd7dc6c8d48b5390
Nicholas-py <74473358+Nicholas-py@users.noreply.github.com>
2024-11-07 14:48:59 -0500

    Update dj.py

--- dj.py
+++ dj.py
@@ -1,28 +1,45 @@
-from setup import *
+from setup import h_it_all, circuitmeasure, simulate
 from qiskit import QuantumCircuit, QuantumRegister,ClassicalRegister
 import random
-def oracle(qc,qs):
-  if random.choice(['balanced','never','EATMOREPIE']) == 'never':
-    print('never')
+def djoracle(qc,qs,type):
+  if type == 'constant':
+    print('Running for a constant function')
     if random.randint(0,1) == 0:
       qc.x(-1)
   else:
-    print('balanced')
+    print('Running for a balanced function')
     for i in range(qs):
       if random.randint(0,1) == 0:
-        #qc.x(i)
-        pass
-    qc.cx(0,qs)
+        qc.x(i)
+        qc.cx(i,qs)
+        qc.x(i)
+      else:
+        qc.cx(i,qs)
 
-def dj(qs):
+def dj(qs,type):
   x = QuantumRegister(qs)
   o = QuantumRegister(1)
   op = ClassicalRegister(qs)
   qc = QuantumCircuit(x,o,op)
   qc.x(qs)
   h_it_all(qc)
-  oracle(qc,qs)
+  djoracle(qc,qs,type)
   h_it_all(qc)
   circuitmeasure(qc,op,x)
-  simulate(qc)
+  a = simulate(qc)
+  if list(a.keys())[0][0] == '1':
+    print('Predicts balanced function')
+  else:
+    print('Predicts constant function')
+  
 
+def djinterface():
+  print('Running Deustch-Jozsa algorithm')
+  qs = int(input('Number of qubits: '))
+  bn = input('Balanced or constant function? ').lower().strip()
+  if 'b' in bn:
+    dj(qs,'balanced')
+  elif 'c' in bn:
+    dj(qs,'constant')
+  else:
+    dj(qs, random.choice(['balanced,constant']))
